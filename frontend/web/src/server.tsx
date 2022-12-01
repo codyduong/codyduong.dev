25e5146c9f5c2b1a1239a32874707ae694f333eb
Cody Duong <duongcody507@gmail.com>
2022-11-30 23:59:43 -0600

    fix favicon

--- frontend/web/src/server.tsx
+++ frontend/web/src/server.tsx
@@ -124,7 +124,7 @@ export const renderApp = (req: express.Request, res: express.Response) => {
       <html lang="">
       <head>
         <meta charset="utf-8" />
-        <link rel="icon" href="favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <meta
