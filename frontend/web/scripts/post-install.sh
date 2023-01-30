61dcd78c8666c5837a392c9c4d4bb4f89064ea96
Cody Duong <duongcody507@gmail.com>
2023-01-29 23:35:00 -0600

    Only yarn generate when not in CI

--- frontend/web/scripts/post-install.sh
+++ frontend/web/scripts/post-install.sh
@@ -10,3 +10,8 @@ if [[ $( cat $reactLib | grep "svg" ) ]]; then
 else
   echo "*.svg module not found in $reactLib"
 fi
+
+# only generate if we are not in CI
+if [[ -z "${CI}" ]]; then
+  yarn graphql-codegen
+fi
