40168f7eabd1bb41f46cf810e5a52eb0e4ed7ae6
Cody Duong <duongcody507@gmail.com>
2023-02-08 12:49:43 -0600

    Update footer design

--- frontend/web/packages/components/Footer/Footer.tsx
+++ frontend/web/packages/components/Footer/Footer.tsx
@@ -2,24 +2,39 @@ import A, { Link } from 'packages/components/A';
 import { cssWidth } from 'packages/components/Section';
 import { LINKS } from 'packages/pages/links/Links';
 import styled, { useTheme } from 'styled-components';
+import T from 'packages/components/Typography';
+import FooterLink from 'packages/components/Footer/FooterLink';
 
 const FooterStyled = styled.footer`
-  background-color: ${({ theme }) => theme.color.surface[350]};
+  // background-color: ${({ theme }) => theme.color.surface[350]};
+  background-color: #2c2c2c;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  box-sizing: border-box;
+`;
 
+const FooterTop = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
   padding: ${({ theme }) =>
-    `${theme.spacing.px[200]} ${theme.spacing.px[150]}`};
+    `${theme.spacing.px[200]} ${theme.spacing.px[150]} ${theme.spacing.px[300]}`};
 `;
 
 const FooterWrapper = styled.div`
+  width: 100%;
   display: flex;
-  flex-grow: 1;
   flex-flow: row wrap;
   justify-content: space-between;
+  align-items: flex-start;
+  gap: ${({ theme }) => theme.spacing.px[200]};
 
-  ${cssWidth}
+  ${cssWidth};
 `;
 
 const LinksWrapper = styled.section`
@@ -27,6 +42,7 @@ const LinksWrapper = styled.section`
   flex-flow: row nowrap;
   color: ${({ theme }) => theme.color.text[100]};
   gap: 8px;
+  margin-right: ${({ theme }) => theme.spacing.px[500]};
 `;
 
 const LinkSVG = styled(A)<{ hovercolor: string }>`
@@ -50,14 +66,46 @@ const LinkSVG = styled(A)<{ hovercolor: string }>`
   }
 `;
 
-const StyledFooterLink = styled(Link)`
-  color: ${({ theme }) => theme.color.text[100]};
-  align-self: center;
+const FooterGroupWrapper = styled.div`
+  display: flex;
+  flex-grow: 1;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  align-items: flex-start;
+  gap: ${({ theme }) => theme.spacing.px[200]};
+`;
 
-  &:hover {
-    cursor: pointer;
-    color: ${({ theme }) => theme.color.base[100]};
-  }
+const FooterGroup = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: ${({ theme }) => theme.spacing.px[50]};
+  flex: 1 1 0;
+  min-width: ${({ theme }) => theme.spacing.px(1250)};
+`;
+
+const FooterHeader = styled.h4`
+  ${T.P2.bold.css}
+  color: ${({ theme }) => theme.color.text[100]};
+  margin: 0 0 0.5rem;
+`;
+
+const Copyright = styled.section`
+  width: 100%;
+  padding: ${({ theme }) => theme.spacing.px[200]};
+  box-sizing: border-box;
+  background-color: ${({ theme }) => theme.color.surface[400]};
+  display: flex;
+  flex-direction: row wrap;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CopyrightText = styled.span`
+  ${T.P4.css}
+  text-align: center;
+  white-space: pre-wrap;
+  color: ${({ theme }) => theme.color.surface[100]};
 `;
 
 const Footer = (): JSX.Element => {
@@ -65,27 +113,49 @@ const Footer = (): JSX.Element => {
 
   return (
     <FooterStyled>
-      <FooterWrapper>
-        <LinksWrapper>
-          {LINKS.map((L) => (
-            <LinkSVG
-              key={L['aria-label']}
-              aria-label={`${L['aria-label']}`}
-              hovercolor={
-                typeof L.hoverColor === 'function'
-                  ? L.hoverColor(theme)
-                  : L.hoverColor
-              }
-              href={L.to}
-            >
-              <L.icon aria-label={`${L['aria-label']}`} />
-            </LinkSVG>
-          ))}
-        </LinksWrapper>
-        <StyledFooterLink to="web-accessibility-statement">
-          Accessibility Statement
-        </StyledFooterLink>
-      </FooterWrapper>
+      <FooterTop>
+        <FooterWrapper>
+          <LinksWrapper>
+            {LINKS.map((L) => (
+              <LinkSVG
+                key={L['aria-label']}
+                aria-label={`${L['aria-label']}`}
+                hovercolor={
+                  typeof L.hoverColor === 'function'
+                    ? L.hoverColor(theme)
+                    : L.hoverColor
+                }
+                href={L.to}
+              >
+                <L.icon aria-label={`${L['aria-label']}`} />
+              </LinkSVG>
+            ))}
+          </LinksWrapper>
+          <FooterGroupWrapper>
+            <FooterGroup>
+              <FooterHeader>Navigate</FooterHeader>
+              <FooterLink to="/home">Home</FooterLink>
+              <FooterLink to="/work">Work</FooterLink>
+              <FooterLink to="/playground">Playground</FooterLink>
+              <FooterLink to="/posts">Posts</FooterLink>
+              <FooterLink to="/contact">Contact</FooterLink>
+            </FooterGroup>
+            <FooterGroup>
+              <FooterHeader>Links</FooterHeader>
+              <FooterLink to="/settings">Manage Settings</FooterLink>
+              <FooterLink to="/web-accessibility-statement">
+                Accessibility Statement
+              </FooterLink>
+              <FooterLink to="/privacy-policy">Privacy Policy</FooterLink>
+            </FooterGroup>
+          </FooterGroupWrapper>
+        </FooterWrapper>
+      </FooterTop>
+      <Copyright>
+        <CopyrightText>
+          © 2023{'    '}|{'    '}Cody Duong{'    '}|{'    '}All Rights Reserved
+        </CopyrightText>
+      </Copyright>
     </FooterStyled>
   );
 };
---
+++ frontend/web/packages/components/Footer/FooterLink.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import { Link } from 'packages/components/A';
+import T from 'packages/components/Typography';
+
+const StyledFooterLink = styled(Link)`
+  font-size: 1.125rem;
+  color: ${({ theme }) => theme.color.text[100]};
+  align-self: center;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  gap: ${({ theme }) => theme.spacing.px[100]};
+  border-bottom: 1px solid ${({ theme }) => theme.color.text[200]};
+  width: 100%;
+
+  &:hover {
+    cursor: pointer;
+    color: ${({ theme }) => theme.color.base[100]};
+  }
+`;
+
+interface FooterLinkProps {
+  to: string;
+  children?: React.ReactNode;
+}
+
+const FooterLink = ({ to, children }: FooterLinkProps): JSX.Element => {
+  return (
+    <StyledFooterLink to={to}>
+      <span>{children}</span>
+      <span aria-hidden>{'>'}</span>
+    </StyledFooterLink>
+  );
+};
+
+export default FooterLink;
